29a77df392567131dc194230198b91cdc83ea22f
40027171 <stephen.jones@health.nsw.gov.au>
2023-12-21 13:53:32 +1100

    working

--- src/components/ForceGraph.js
+++ src/components/ForceGraph.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { ForceGraph2D } from 'react-force-graph';
 
 const ForceGraph = ({ data }) => {
+    const fgRef = useRef();
+
+    useEffect(() => {
+        // Setup force graph configuration
+        const forceGraph = fgRef.current;
+        forceGraph.d3Force('charge').strength(-300); // Increased repulsive force
+        // You can also adjust other forces as needed
+    }, []);
+
     const handleNodePaint = (node, ctx) => {
         // draw the node (a circle)
         ctx.beginPath();
@@ -15,16 +24,22 @@ const ForceGraph = ({ data }) => {
         ctx.textBaseline = 'middle';
         ctx.fillStyle = 'black';
         ctx.fillText(node.id, node.x, node.y - 15);
-
-        // You can add more complex drawing here if needed
     };
 
+    const handleNodeClick = node => {
+        // Open a new tab with the URL from the node data
+        if (node.url) {
+            window.open(node.url, '_blank');
+        }
+    };
 
     return (
         <ForceGraph2D
+            ref={fgRef}
             graphData={data}
             nodeCanvasObject={handleNodePaint}
             nodeAutoColorBy="group"
+            onNodeClick={handleNodeClick}
             linkDirectionalParticles="value"
             linkDirectionalParticleSpeed={d => d.value * 0.001}
         />
--- src/data/graphData.js
+++ src/data/graphData.js
@@ -1,14 +1,14 @@
 const graphData = {
     nodes: [
-      { id: 'Essential 8', group: 1 },
-      { id: 'Managed Services', group: 2 },
-      { id: 'Immutable infrastructure', group: 2 },
-      { id: 'Mutable infrastructure', group: 2 },
-      { id: 'Identities', group: 2 },
-      { id: 'Data perimeter', group: 2 },
-      { id: 'Backups', group: 2 },
-      { id: 'Logging and monitoring', group: 2 },
-      { id: 'Mechanisms for manual processes', group: 2 }
+      { id: 'Essential 8', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/introduction.html', group: 1 },
+      { id: 'Managed Services', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-1.html', group: 2 },
+      { id: 'Immutable infrastructure', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-2.html', group: 2 },
+      { id: 'Mutable infrastructure', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-3.html', group: 2 },
+      { id: 'Identities', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-4.html', group: 2 },
+      { id: 'Data perimeter', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-5.html', group: 2 },
+      { id: 'Backups', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-6.html', group: 2 },
+      { id: 'Logging and monitoring', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-7.html', group: 2 },
+      { id: 'Mechanisms for manual processes', expanded: false, url: 'https://docs.aws.amazon.com/prescriptive-guidance/latest/essential-eight-maturity/theme-8.html', group: 2 }
     ],
     links: [
       { source: 'Essential 8', target: 'Managed Services' },
@@ -23,4 +23,3 @@ const graphData = {
   };
   
   export default graphData;
-  
